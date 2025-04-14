bac351ded17ba2bfffc3fb1511fb75608c4ebba9
Pratik <pratikkumar1225@gmail.com>
2025-04-15 01:01:22 +0530

    Updated Webpage

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
 # Load model
-model = pickle.load(open('model.pkl', 'rb'))
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/')
-def home():
-    return "Flask app is working!"
+def index():
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['data']  # expects JSON: {"data": [values]}
-    prediction = model.predict([np.array(data)])
+    data = request.get_json(force=True)
+    input_features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(input_features)
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
